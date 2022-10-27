6734f8c5acd978b9767f05f28356dba72b0ed905
Anushka Darade <darade.a@northeastern.edu>
2022-10-27 19:12:39 -0400

    patient and community

---
+++ src/model/Person.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Subhash
+ */
+public class Person extends Residence{
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    @Override
+    public String toString()
+    {
+        return Name;
+    }
+    protected String Name;
+    protected int age;
+    protected String Gender;
+}
